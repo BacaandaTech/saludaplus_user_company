8962228831ac5414ca517e87747f97a0ad276f0c
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-02-13 18:25:30 -0600

    fix in url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL:'http://164.92.96.185/api/v1/',
+    API_URL:'https://apisandbox.saludaplus.com/api/',
     KEY_ENCRYPT:'5540dd1b-8340-4b30-803b-4823743b1511',
     STRIPE_PUBLIC: ''
 };
