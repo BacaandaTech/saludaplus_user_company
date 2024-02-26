e69f08d67cfb17eea9c9c342e0d71cf08dc76ae1
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-02-26 16:36:28 -0600

    fix in keys environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    API_URL:'https://apisandbox.saludaplus.com/api/',
+    API_URL:'https://api.saludaplus.com/api/v1/',
     KEY_ENCRYPT:'5540dd1b-8340-4b30-803b-4823743b1511',
-    STRIPE_PUBLIC: ''
+    STRIPE_PUBLIC: 'pk_live_51Nws6oCC6BCplg6nTAtTBpzAnfx91LmNExuXaqtkmDPQr4MjdevDvqHM41Wq6qFjppGoecy4FFGU50156PuEbPWf00hPDylulU',
+    STRIPE_SECRET: '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'
 };
