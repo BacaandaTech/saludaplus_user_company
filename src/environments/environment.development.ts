45dbf1c385982100be8f74763ae2f099c1b1d28f
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-02-26 19:33:19 -0600

    deleting environments

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    API_URL:'https://apisandbox.saludaplus.com/api/v1/',
-    KEY_ENCRYPT:'5540dd1b-8340-4b30-803b-4823743b1511',
-    STRIPE_PUBLIC:'pk_test_51Nws6oCC6BCplg6nYRo7g9I9hykBN6j1uCcN7ULVXTkuWR4MsJVulHC0r6a1biCJKJ27o1mAH7RU8eAHKmkY36GT00a7DPQQzI',
-};
